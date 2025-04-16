46df053c7a4b79ee006be35ad3e033dc7f87a583
Rodrigo-555 <sm60771457@idat.pe>
2025-04-16 03:05:02 -0500

    agregado de la pantalla de carga

--- app/src/main/java/com/rodrigotocto/myappfirebaseauth/LoadActivity.kt
+++ app/src/main/java/com/rodrigotocto/myappfirebaseauth/LoadActivity.kt
@@ -1,6 +1,9 @@
 package com.rodrigotocto.myappfirebaseauth
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,12 +14,18 @@ class LoadActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_load)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Espera 1.5 segundos y luego lanza la siguiente actividad
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, SignInActivity::class.java) // O MainActivity
+            startActivity(intent)
+            finish()
+        }, 1500) // 1500 milisegundos = 1.5 segundos
     }
-
-
 }
