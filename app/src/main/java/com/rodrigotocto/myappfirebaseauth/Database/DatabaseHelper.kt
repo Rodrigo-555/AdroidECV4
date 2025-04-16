f7c20135087c97240060fd8a98ffd0ca815b6f3d
AndyHs25 <sm72181196@idat.edu.pe>
2025-04-15 23:16:41 -0500

    agregado favoritos y usuarios entidades

--- app/src/main/java/com/rodrigotocto/myappfirebaseauth/Database/DatabaseHelper.kt
+++ app/src/main/java/com/rodrigotocto/myappfirebaseauth/Database/DatabaseHelper.kt
@@ -18,6 +18,18 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
         const val COLUMN_PRECIO = "precio"
         const val COLUMN_IMAGEN = "imagen"
         const val COLUMN_TIPO = "tipo"
+
+        // Tabla Usuarios
+        const val TABLE_USERS = "usuarios"
+        const val COLUMN_USER_ID = "user_id"
+        const val COLUMN_EMAIL = "email"
+
+        // Tabla Favoritos
+        const val TABLE_FAVORITES = "favoritos"
+        const val COLUMN_FAVORITE_ID = "favorite_id"
+        const val COLUMN_PRODUCT_ID = "product_id"
+
+
     }
 
     override fun onCreate(db: SQLiteDatabase) {
@@ -33,12 +45,32 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
             )
         """.trimIndent()
 
+        val CREATE_USERS_TABLE = """
+            CREATE TABLE $TABLE_USERS (
+                $COLUMN_USER_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                $COLUMN_EMAIL TEXT NOT NULL
+            )
+        """.trimIndent()
+
+        val CREATE_FAVORITES_TABLE = """
+            CREATE TABLE $TABLE_FAVORITES (
+                $COLUMN_FAVORITE_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                $COLUMN_USER_ID INTEGER NOT NULL,
+                $COLUMN_PRODUCT_ID INTEGER NOT NULL
+            )
+        """.trimIndent()
+
+
         db.execSQL(CREATE_PRODUCTOS_TABLE)
+        db.execSQL(CREATE_USERS_TABLE)
+        db.execSQL(CREATE_FAVORITES_TABLE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         // En caso de actualización, eliminar la tabla y recrearla
         db.execSQL("DROP TABLE IF EXISTS $TABLE_PRODUCTOS")
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_USERS")
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_FAVORITES")
         onCreate(db)
     }
 }
